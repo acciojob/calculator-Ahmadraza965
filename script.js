302e15d39978d9df06362eef701d609cb45b6355
Ahmadraza965 <120107650+Ahmadraza965@users.noreply.github.com>
2023-01-10 02:00:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your code here
+const input = document.getElementById("input");
+const clr = document.getElementById("clr");
+const divide = document.getElementById("divide");
+const multiply = document.getElementById("multiply");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const dot = document.getElementById("dot");
+const ans = document.getElementById("ans");
+
+const blocks = document.querySelectorAll(".block");
+
+let currentValue = "0";
+let operator = "";
+let previousValue = "";
+
+blocks.forEach((block) => {
+  block.addEventListener("click", (e) => {
+    const value = e.target.textContent;
+    if (value === "C") {
+      currentValue = "0";
+      operator = "";
+      previousValue = "";
+    } else if (value === "." && !currentValue.includes(".")) {
+      currentValue += ".";
+    } else if (value === "=") {
+      previousValue = operate(previousValue, currentValue, operator);
+      currentValue = previousValue;
+      operator = "";
+    } else if (["+", "-", "&times;", "&divide;"].includes(value)) {
+      previousValue = currentValue;
+      currentValue = "0";
+      operator = value;
+    } else {
+      if (currentValue === "0") {
+        currentValue = value;
+      } else {
+        currentValue += value;
+      }
+    }
+    input.value = currentValue;
+  });
+});
+
+
